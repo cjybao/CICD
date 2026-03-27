44cf5e1281bb1ecc82d8e5b904aa12def7b26dff
ccjy <61445855+cjybao@users.noreply.github.com>
2026-03-27 16:42:01 +0800

    Update VulnerableClass.java

--- src/main/java/com/example/VulnerableClass.java
+++ src/main/java/com/example/VulnerableClass.java
@@ -8,16 +8,16 @@ import java.sql.Statement;
 
 public class VulnerableClass {
     public void doGet(HttpServletRequest request) throws Exception {
-        String userInput = request.getParameter("name");  // 污点源
+        String userInput1 = request.getParameter("name1");  // 污点源
         String url = "jdbc:mysql://localhost:3306/test";
         String username = "root";
         String password = "password";
-        String query = "SELECT * FROM users WHERE name = '" + userInput + "'";
+        String query = "SELECT * FROM users WHERE name = '" + userInput1 + "'";
         try (Connection conn = DriverManager.getConnection(url, username, password);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
             while (rs.next()) {
-                System.out.println("User: " + rs.getString("name"));
+                System.out.println("User: " + rs.getString("name1"));
             }
         }
     }
